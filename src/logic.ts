1889b94517bcd7bc7b3e870f8d284f835e146cd3
Notan <49018674+NotaninDev@users.noreply.github.com>
2023-11-19 22:29:46 +0900

    Implement Board class

--- src/logic.ts
+++ src/logic.ts
@@ -1,12 +1,36 @@
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left
+}
+
 class Cell {
     boost: boolean = false;
     mirrorUpRight: null | boolean = false;
     bell: null | number = null;
+    generator: null | Direction = null;
 
     hasMirror(): boolean {
         return this.mirrorUpRight !== null;
     }
     hasBell(): boolean {
         return this.bell !== null;
+    }
+}
+
+export class Board {
+    size: [number, number]; // row, column
+    cells: Cell[][];
+
+    constructor(size: [number, number]) {
+        this.size = size;
+        this.cells = [];
+        for (let i = 0; i < size[0]; i++) {
+            this.cells[i] = [];
+            for (let j = 0; j < size[1]; j++) {
+                this.cells[i][j] = new Cell();
+            }
+        }
     }
 }
